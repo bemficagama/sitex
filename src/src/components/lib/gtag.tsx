d1a650ae06eb7906019fef2091b72a1550676719
André Bemfica <andre@microben.com.br>
2022-10-12 13:43:09 -0400

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/src/components/lib/gtag.tsx

--- src/src/components/lib/gtag.tsx
+++ src/src/components/lib/gtag.tsx
@@ -1,17 +1,23 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url :any) => {
+export const pageview = (url: any) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value } : any) => {
+export const event = ({ action, category, label, value }: any) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
     value: value,
   })
+}
+
+declare global {
+  interface Window {
+    gtag: any; // 👈️ turn off type checking
+  }
 }
