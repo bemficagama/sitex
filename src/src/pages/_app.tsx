714b3d60901231ad2701725ab056c905ead052ff
André Bemfica <andre@microben.com.br>
2022-10-11 22:01:20 -0400

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/env.default
    	new file:   src/src/components/Analytics.tsx
    	new file:   src/src/components/lib/gtag.tsx
    	modified:   src/src/pages/_app.tsx

--- src/src/pages/_app.tsx
+++ src/src/pages/_app.tsx
@@ -4,14 +4,35 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import { useEffect } from "react"
+import { useRouter } from 'next/router'
+
+import * as gtag from "../components/lib/gtag"
+import Analytics from '../components/Analytics'
 
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, [])
-  return <Component {...pageProps} />
+
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+
+  }, [router.events])
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default MyApp
